860bb23754c686f8a686bca13878f270c61bf7ee
Balázs János Varga <vargab95@gmail.com>
2020-02-29 23:15:13 +0100

    Minor fixes

--- detector.py
+++ detector.py
@@ -8,18 +8,17 @@ import actions
 
 class CrossOverDetector:
     def __init__(self, bullish: Callable, bearish: Callable):
-        self.previous_summary: float = None
+        self.previous_summary: float = 0.0
         self.bullish_callback: Callable = bullish
         self.bearish_callback: Callable = bearish
 
     def check_crossover(self, summary: float) -> actions.TradingAction:
         logging.info("Current state: %f", summary)
-        if self.previous_summary:
-            if summary == 0.0 and self.previous_summary == 0.0:
-                pass
-            if summary >= 0.0 and self.previous_summary <= 0.0:
-                self.bullish_callback()
-            elif summary <= 0.0 and self.previous_summary >= 0.0:
-                self.bearish_callback()
+        if summary == 0.0 and self.previous_summary == 0.0:
+            pass
+        if summary >= 0.0 and self.previous_summary <= 0.0:
+            self.bullish_callback()
+        elif summary <= 0.0 and self.previous_summary >= 0.0:
+            self.bearish_callback()
 
         self.previous_summary = summary
