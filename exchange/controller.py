f619369edf849aa2169e2e0997bbd677e3d2a0ab
Balázs János Varga <vargab95@gmail.com>
2020-02-29 19:42:19 +0100

    Add price getter functionality and log it in every cycle.

--- exchange/controller.py
+++ exchange/controller.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
+import binance.client
+
+import config
 import exchange.interface
 
 class BinanceController(exchange.interface.ExchangeInterface):
+    def __init__(self, config: config.ExchangeConfig):
+        self.client = binance.client.Client(config.public_key, config.private_key)
+
     def buy(market_name: str, amount: float) -> bool:
         pass
 
@@ -12,5 +18,5 @@ class BinanceController(exchange.interface.ExchangeInterface):
     def get_balances(self) -> exchange.interface.Balances:
         pass
 
-    def get_price(self, market_name: str):
-        pass
+    def get_price(self, market_name: str) -> float:
+        return float(self.client.get_ticker(symbol="BTCUSDT")["lastPrice"])
