c51aa735e2c8027bcc097fb9f39113f325915e4c
Balázs János Varga <vargab95@gmail.com>
2020-03-03 17:06:40 +0100

    Fix bugs of crossover detector.

--- detector.py
+++ detector.py
@@ -1,26 +1,40 @@
 #!/usr/bin/python3
 
 import logging
+import enum
 
 from typing import Callable
 
 import actions
 
+class CurrentState(enum.Enum):
+    NONE = 0
+    BEAR = 1
+    BULL = 2
+
 class CrossOverDetector:
-    def __init__(self, bullish_threshold: float = 0.0, bearish_threshold: float = 0.0):
-        self.previous_summary: float = 0.0
+    def __init__(self, bearish_threshold: float = 0.0, bullish_threshold: float = 0.0):
+        self.current_state: CurrentState = CurrentState.NONE
         self.bullish_threshold: float = bullish_threshold
         self.bearish_threshold: float = bearish_threshold
+        self.previous_summary: float = (bullish_threshold + bearish_threshold) / 2
 
     def check_crossover(self, summary: float) -> actions.TradingAction:
         result = actions.TradingAction.HOLD
         logging.info("Current state: %f", summary)
-        if summary > self.bullish_threshold and self.previous_summary <= self.bullish_threshold:
+        print (summary, self.bearish_threshold, self.previous_summary, self.current_state)
+        if summary > self.bullish_threshold and \
+                self.previous_summary <= self.bullish_threshold and \
+                self.current_state != CurrentState.BULL:
             logging.debug("Bullish trade was detected.")
             result = actions.TradingAction.SWITCH_TO_BULLISH
-        elif summary < self.bearish_threshold and self.previous_summary >= self.bearish_threshold:
+            self.current_state = CurrentState.BULL
+        elif summary < self.bearish_threshold and \
+                self.previous_summary >= self.bearish_threshold and \
+                self.current_state != CurrentState.BEAR:
             logging.debug("Bearish trade was detected.")
             result = actions.TradingAction.SWITCH_TO_BEARISH
+            self.current_state = CurrentState.BEAR
 
         self.previous_summary = summary
         return result
