4f547bbf539b6f631729781c75991c1d85be3be0
Balázs János Varga <vargab95@gmail.com>
2020-03-05 18:18:47 +0100

    Reverse detector was implemented. refs #747

--- detector/reverse_crossover.py
+++ detector/reverse_crossover.py
@@ -3,10 +3,11 @@
 import logging
 
 import actions
+import detector.common
 
 class ReverseCrossOverDetector:
     def __init__(self, bearish_threshold: float = 0.0, bullish_threshold: float = 0.0):
-        self.current_state: CurrentState = CurrentState.NONE
+        self.current_state: detector.common.CurrentState = detector.common.CurrentState.NONE
         self.bullish_threshold: float = bullish_threshold
         self.bearish_threshold: float = bearish_threshold
         self.previous_summary: float = (bullish_threshold + bearish_threshold) / 2
@@ -14,18 +15,18 @@ class ReverseCrossOverDetector:
     def check(self, summary: float) -> actions.TradingAction:
         result = actions.TradingAction.HOLD
         logging.info("Current state: %f", summary)
-        if summary > self.bullish_threshold and \
-                self.previous_summary <= self.bullish_threshold and \
-                self.current_state != CurrentState.BULL:
+        if summary < self.bullish_threshold and \
+                self.previous_summary >= self.bullish_threshold and \
+                self.current_state != detector.common.CurrentState.BULL:
             logging.debug("Bullish trade was detected.")
             result = actions.TradingAction.SWITCH_TO_BULLISH
-            self.current_state = CurrentState.BULL
-        elif summary < self.bearish_threshold and \
-                self.previous_summary >= self.bearish_threshold and \
-                self.current_state != CurrentState.BEAR:
+            self.current_state = detector.common.CurrentState.BULL
+        elif summary > self.bearish_threshold and \
+                self.previous_summary <= self.bearish_threshold and \
+                self.current_state != detector.common.CurrentState.BEAR:
             logging.debug("Bearish trade was detected.")
             result = actions.TradingAction.SWITCH_TO_BEARISH
-            self.current_state = CurrentState.BEAR
+            self.current_state = detector.common.CurrentState.BEAR
 
         self.previous_summary = summary
         return result
