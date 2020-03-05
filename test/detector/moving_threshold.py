6eb4c890d28383db0e29fd03b2a59a0d69262a94
Balázs János Varga <vargab95@gmail.com>
2020-03-05 19:12:17 +0100

    Follower detector was implemented. refs #748

--- test/detector/moving_threshold.py
+++ test/detector/moving_threshold.py
@@ -16,7 +16,7 @@ class CrossOverDetectorTest(unittest.TestCase):
     def setUp(self):
         self.spider = SpiderMock()
 
-    def test_quick_fall_down(self):
+    def test_quick_fall_down_zero_threshold(self):
         test_detector = detector.moving_threshold.MovingThresholdCrossOverDetector(0.0, 0.0, self.spider)
         test_data = [
             [-0.066667, actions.TradingAction.SWITCH_TO_BEARISH],
@@ -27,5 +27,39 @@ class CrossOverDetectorTest(unittest.TestCase):
             [-0.378788, actions.TradingAction.HOLD],
             [-0.378789, actions.TradingAction.HOLD]
         ]
+        for line in test_data:
+            self.assertEqual(test_detector.check(line[0]), line[1], str(line))
+
+    def test_quick_fall_down_below_threshold(self):
+        test_detector = detector.moving_threshold.MovingThresholdCrossOverDetector(0.0, 0.0, self.spider)
+        test_data = [
+            [-0.066667, actions.TradingAction.HOLD],
+            [-0.066668, actions.TradingAction.HOLD],
+            [-0.021212, actions.TradingAction.HOLD],
+            [0.0, actions.TradingAction.HOLD],
+            [0.0, actions.TradingAction.HOLD],
+            [-0.378788, actions.TradingAction.HOLD],
+            [-0.378789, actions.TradingAction.HOLD]
+        ]
+
+        self.spider.summary = -0.1
+
+        for line in test_data:
+            self.assertEqual(test_detector.check(line[0]), line[1], str(line))
+
+    def test_quick_fall_down_above_threshold(self):
+        test_detector = detector.moving_threshold.MovingThresholdCrossOverDetector(0.0, 0.0, self.spider)
+        test_data = [
+            [-0.066667, actions.TradingAction.HOLD],
+            [-0.066668, actions.TradingAction.HOLD],
+            [-0.021212, actions.TradingAction.HOLD],
+            [0.0, actions.TradingAction.HOLD],
+            [0.0, actions.TradingAction.HOLD],
+            [-0.378788, actions.TradingAction.SWITCH_TO_BEARISH],
+            [-0.378789, actions.TradingAction.HOLD]
+        ]
+
+        self.spider.summary = 0.1
+
         for line in test_data:
             self.assertEqual(test_detector.check(line[0]), line[1], str(line))
