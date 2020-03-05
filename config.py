02820a05050230c9a619c7ca65fc9919360fe602
Balázs János Varga <vargab95@gmail.com>
2020-03-05 20:15:46 +0100

    Add balance store parameter for storing testing balances in a persistent storage to preserve in case of failures.

--- config.py
+++ config.py
@@ -23,13 +23,15 @@ class TestingConfig:
         self.real_time: bool = config.get("real_time", False)
         self.start_money: float = config.get("start_money", 100.0)
         self.fee: float = config.get("fee", 0.001)
+        self.balance_store: str = config.get("balance_store", "balances.json")
 
     def __str__(self):
         return "\nTesting:" + \
-               "\n    Enabled:     " + str(self.enabled) + \
-               "\n    Real time:   " + str(self.real_time) + \
-               "\n    Fee:         " + str(self.fee) + \
-               "\n    Start money: " + str(self.start_money)
+               "\n    Enabled:        " + str(self.enabled) + \
+               "\n    Real time:      " + str(self.real_time) + \
+               "\n    Fee:            " + str(self.fee) + \
+               "\n    Balances store: " + self.balance_store + \
+               "\n    Start money:    " + str(self.start_money)
 
 class ExchangeConfig:
     def __init__(self, config: typing.Dict):
