473caa21e2016307e9d124868fa8f38bc30cfccf
Balázs János Varga <vargab95@gmail.com>
2020-03-05 19:12:17 +0100

    Follower detector was implemented. refs #748

--- detector/moving_threshold.py
+++ detector/moving_threshold.py
@@ -5,29 +5,27 @@ import logging
 import spider
 import actions
 import detector.common
+import detector.crossover
 
-class MovingThresholdCrossOverDetector:
+class MovingThresholdCrossOverDetector(detector.crossover.CrossOverDetector):
     def __init__(self, bearish_threshold: float, bullish_threshold: float, spider: spider.TradingViewSpider):
-        self.current_state: detector.common.CurrentState = detector.common.CurrentState.NONE
-        self.bullish_threshold: float = bullish_threshold
-        self.bearish_threshold: float = bearish_threshold
-        self.previous_summary: float = (bullish_threshold + bearish_threshold) / 2
+        super().__init__(bearish_threshold, bullish_threshold)
+        self.original_bearish_threshold = bearish_threshold
+        self.original_bullish_threshold = bullish_threshold
+        self.spider = spider
+
+    @staticmethod
+    def __saturate(value: float, threshold: float) -> float:
+        if value > threshold:
+            return threshold
+        if value < (threshold * -1):
+            return threshold * -1
+        return value
 
     def check(self, summary: float) -> actions.TradingAction:
-        result = actions.TradingAction.HOLD
-        logging.info("Current state: %f", summary)
-        if summary > self.bullish_threshold and \
-                self.previous_summary <= self.bullish_threshold and \
-                self.current_state != detector.common.CurrentState.BULL:
-            logging.debug("Bullish trade was detected.")
-            result = actions.TradingAction.SWITCH_TO_BULLISH
-            self.current_state = detector.common.CurrentState.BULL
-        elif summary < self.bearish_threshold and \
-                self.previous_summary >= self.bearish_threshold and \
-                self.current_state != detector.common.CurrentState.BEAR:
-            logging.debug("Bearish trade was detected.")
-            result = actions.TradingAction.SWITCH_TO_BEARISH
-            self.current_state = detector.common.CurrentState.BEAR
+        long_term_summary = self.spider.get_technical_summary()
 
-        self.previous_summary = summary
-        return result
+        self.bearish_threshold = self.__saturate(self.original_bearish_threshold - long_term_summary, 1.0)
+        self.bullish_threshold = self.__saturate(self.original_bullish_threshold - long_term_summary, 1.0)
+
+        return super().check(summary)
