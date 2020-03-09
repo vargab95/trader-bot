6a1a77aa7fd065196b31be3ce44c0a7cd5c8dfe2
Balázs János Varga <me@vbj.hu>
2020-03-09 18:26:20 +0000

    Introduce SMA and falling edge detector.

--- config/market.py
+++ config/market.py
@@ -8,17 +8,21 @@ class MarketConfig:
         self.name = config.get("name", "GEMINI:BTCUSD")
         self.check_interval = config.get("check_interval", 60)
         self.candle_size = config.get("candle_size", "1h")
+        self.summary_sma = config.get("summary_sma", 1200)
         self.follower_enabled = config.get("follower_enabled", False)
         self.follower_candle_size = config.get("follower_candle_size", "1h")
+        self.falling_edge_detection = config.get("falling_edge_detection", False)
         self.bullish_threshold = config.get("bullish_threshold", 0.0)
         self.bearish_threshold = config.get("bearish_threshold", 0.0)
 
     def __str__(self):
         return "\nMarket:" + \
-               "\n    Name:                 " + self.name + \
-               "\n    Candle size:          " + self.candle_size + \
-               "\n    Follower enabled:     " + str(self.follower_enabled) + \
-               "\n    Follower candle size: " + self.follower_candle_size + \
-               "\n    Check interval:       " + str(self.check_interval) + \
-               "\n    Bullish threshold:    " + str(self.bullish_threshold) + \
-               "\n    Bearish threshold:    " + str(self.bearish_threshold)
+               "\n    Name:                   " + self.name + \
+               "\n    Candle size:            " + self.candle_size + \
+               "\n    Summary SMA:            " + str(self.summary_sma) + \
+               "\n    Follower enabled:       " + str(self.follower_enabled) + \
+               "\n    Follower candle size:   " + self.follower_candle_size + \
+               "\n    Check interval:         " + str(self.check_interval) + \
+               "\n    Falling edge detection: " + str(self.falling_edge_detection) + \
+               "\n    Bullish threshold:      " + str(self.bullish_threshold) + \
+               "\n    Bearish threshold:      " + str(self.bearish_threshold)
