a5eceb6da6ff6127149637f1e99fe3746622ca90
Balázs János Varga <me@vbj.hu>
2020-03-09 16:24:42 +0000

    Fix moving threshold issue. It handled the moving threshold wrongly if it jumped through a threshold.

--- detector/crossover.py
+++ detector/crossover.py
@@ -12,20 +12,16 @@ class CrossOverDetector:
             detector.common.CurrentState.NONE
         self.bullish_threshold: float = bullish_threshold
         self.bearish_threshold: float = bearish_threshold
-        self.previous_summary: float = (bullish_threshold +
-                                        bearish_threshold) / 2
 
     def check(self, summary: float) -> actions.TradingAction:
         result = actions.TradingAction.HOLD
         logging.info("Current state: %f", summary)
         if summary > self.bullish_threshold and \
-                self.previous_summary <= self.bullish_threshold and \
                 self.current_state != detector.common.CurrentState.BULL:
             logging.debug("Bullish trade was detected.")
             result = actions.TradingAction.SWITCH_TO_BULLISH
             self.current_state = detector.common.CurrentState.BULL
         elif summary < self.bearish_threshold and \
-                self.previous_summary >= self.bearish_threshold and \
                 self.current_state != detector.common.CurrentState.BEAR:
             logging.debug("Bearish trade was detected.")
             result = actions.TradingAction.SWITCH_TO_BEARISH
