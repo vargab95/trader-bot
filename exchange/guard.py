9f6f07929cd2355faade257ee8896b8b123b1833
Balázs János Varga <vargab95@gmail.com>
2020-03-14 08:24:22 +0100

    Add exchange guard for netword operations.

---
+++ exchange/guard.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import logging
+
+import binance.exceptions
+
+
+def exchange_guard(function):
+    def decorator(*args, **kwargs):
+        for _ in range(5):
+            try:
+                return function(*args, **kwargs)
+            except binance.exceptions.BinanceAPIException as api_exception:
+                logging.error(
+                    "Exception occured during exchange operation %s: %s",
+                    function.__name__, str(api_exception))
+        return False
+
+    return decorator
---
+++ exchange/ut_guard.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import unittest
+
+import binance.exceptions
+
+import exchange.guard
+
+
+class response_mock:
+    status_code = 1
+
+    @staticmethod
+    def json():
+        return {"code": "1", "msg": "1"}
+
+
+class ExchangeGuardTest(unittest.TestCase):
+    @exchange.guard.exchange_guard
+    def test_function(self):
+        if self.raise_until > 0:
+            self.raise_until -= 1
+            raise binance.exceptions.BinanceAPIException(response_mock())
+        return True
+
+    def setUp(self):
+        self.raise_until = -1
+
+    def test_normal_flow(self):
+        self.assertTrue(self.test_function())
+
+    def test_temporary_failure(self):
+        self.raise_until = 3
+        self.assertTrue(self.test_function())
+
+    def test_permanent_failure(self):
+        self.raise_until = 6
+        self.assertFalse(self.test_function())
