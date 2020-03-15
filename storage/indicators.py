77cea898c10149441eb4b14fe87485db3059c8f2
Balázs János Varga <vargab95@gmail.com>
2020-03-15 08:39:26 +0100

    Implement data exporter. refs #751

--- storage/indicators.py
+++ storage/indicators.py
@@ -15,17 +15,39 @@ class IndicatorsStorage:
             "value": value
         })
 
-    def get(self, market, candle_size, start_date, end_date):
+    def get(self,
+            market="",
+            candle_size="",
+            start_date=None,
+            end_date=None,
+            limit=-1):
+
+        if not market or not candle_size:
+            return None
+
         market_collection = self.__collection[market]
         candle_collection = market_collection[candle_size]
+
+        result = None
         if start_date and end_date:
-            return candle_collection.find(
-                {'time': {
+            result = candle_collection.find(
+                {'date': {
                     '$gte': start_date,
                     '$lt': end_date
                 }})
-        if start_date:
-            return candle_collection.find({'time': {
+        elif start_date:
+            result = candle_collection.find({'date': {
                 '$gte': start_date,
             }})
-        return candle_collection.find({'time': {'$lt': end_date}})
+        elif end_date:
+            result = candle_collection.find({'time': {'$lt': end_date}})
+        else:
+            result = candle_collection.find()
+
+        if limit > 0:
+            result = result.sort([("timestamp", -1)]).limit(limit)
+
+        return [{
+            "date": line["date"],
+            "value": line["value"]
+        } for line in result]
--- storage/tickers.py
+++ storage/tickers.py
@@ -14,16 +14,33 @@ class TickersStorage:
             "price": price
         })
 
-    def get(self, market, start_date, end_date):
+    def get(self, market="", start_date=None, end_date=None, limit=-1):
+
+        if not market:
+            return None
+
         market_collection = self.__collection[market]
+
+        result = None
         if start_date and end_date:
-            return market_collection.find(
-                {'time': {
+            result = market_collection.find(
+                {'date': {
                     '$gte': start_date,
                     '$lt': end_date
                 }})
-        if start_date:
-            return market_collection.find({'time': {
+        elif start_date:
+            result = market_collection.find({'date': {
                 '$gte': start_date,
             }})
-        return market_collection.find({'time': {'$lt': end_date}})
+        elif end_date:
+            result = market_collection.find({'time': {'$lt': end_date}})
+        else:
+            result = market_collection.find()
+
+        if limit > 0:
+            result = result.sort([("timestamp", -1)]).limit(limit)
+
+        return [{
+            "date": line["date"],
+            "price": line["price"]
+        } for line in result]
