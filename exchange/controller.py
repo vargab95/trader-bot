910f1b7dcd2d6992e4a75c112139b2c2beda9c3c
Balázs János Varga <me@vbj.hu>
2020-03-21 12:34:30 +0000

    Fix connection, mailing and insufficient resource bugs. refs #772

--- exchange/controller.py
+++ exchange/controller.py
@@ -16,14 +16,18 @@ class BinanceController(exchange.interface.ExchangeInterface):
                                             configuration.private_key)
         exchange_info = self.client.get_exchange_info()
         self.__min_amount = {}
+        self.__min_notional = {}
         logging.debug("Min quantities:")
         for market_info in exchange_info["symbols"]:
             for filter_info in market_info["filters"]:
                 if filter_info["filterType"] == "LOT_SIZE":
                     min_quantity = float(filter_info["minQty"])
+                if filter_info["filterType"] == "MIN_NOTIONAL":
+                    min_notional = float(filter_info["minNotional"])
             symbol = market_info["symbol"]
             self.__min_amount[symbol] = min_quantity
-            logging.debug("    %s: %f", symbol, min_quantity)
+            self.__min_notional[symbol] = min_notional
+            logging.debug("    %s: LOT_SIZE: %f MIN_NOTIONAL: %f", symbol, min_quantity, min_notional)
 
     @exchange.guard.exchange_guard
     def buy(self, market: exchange.interface.Market, amount: float) -> bool:
@@ -31,14 +35,16 @@ class BinanceController(exchange.interface.ExchangeInterface):
         logging.info("Trying to buy %.10f %s", corrected_amount, market.key)
         logging.debug("%.10f was corrected to %.10f", amount, corrected_amount)
 
-        if corrected_amount < self.__min_amount[market.key]:
+        if not self.__is_enough_amount(market, corrected_amount):
             logging.warning("Buy failed due to insufficient resources.")
             return False
 
+        correted_amount_str = "{:.12f}".format(corrected_amount).rstrip('0')
+        logging.debug("Corrected amount string: %s", correted_amount_str)
         self.client.create_order(symbol=market.key,
                                  side=binance.client.Client.SIDE_BUY,
                                  type=binance.client.Client.ORDER_TYPE_MARKET,
-                                 quantity=str(corrected_amount))
+                                 quantity=correted_amount_str)
         logging.info("%.10f %s was successfully bought", corrected_amount,
                      market.key)
         return True
@@ -49,16 +55,28 @@ class BinanceController(exchange.interface.ExchangeInterface):
         logging.info("Trying to sell %.10f %s", corrected_amount, market.key)
         logging.debug("%.10f was corrected to %.10f", amount, corrected_amount)
 
-        if corrected_amount < self.__min_amount[market.key]:
+        if not self.__is_enough_amount(market, corrected_amount):
             logging.warning("Sell failed due to insufficient resources.")
             return False
 
+        correted_amount_str = "{:.12f}".format(corrected_amount).rstrip('0')
+        logging.debug("Corrected amount string: %s", correted_amount_str)
         self.client.create_order(symbol=market.key,
                                  side=binance.client.Client.SIDE_SELL,
                                  type=binance.client.Client.ORDER_TYPE_MARKET,
-                                 quantity=str(corrected_amount))
+                                 quantity=correted_amount_str)
         logging.info("%.10f %s was successfully sold", corrected_amount,
                      market.key)
+        return True
+
+    def __is_enough_amount(self, market, amount: float):
+        if amount < self.__min_amount[market.key]:
+            return False
+
+        price = self.get_price(market)
+        if amount < (self.__min_notional[market.key] / price):
+            return False
+
         return True
 
     @exchange.guard.exchange_guard
@@ -108,4 +126,4 @@ class BinanceController(exchange.interface.ExchangeInterface):
         if remainder > (0.5 * precision):
             result -= precision
         result = round(result, exponent)
-        return result
+        return abs(result)
