99c669c8edbe8a35387309f396ee11e5834ba6cc
Balázs János Varga <me@vbj.hu>
2020-03-21 12:34:30 +0000

    Fix connection, mailing and insufficient resource bugs. refs #772

--- exchange/guard.py
+++ exchange/guard.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 
+import time
 import logging
+import json
 
 import binance.exceptions
-
+import requests.exceptions
 
 def exchange_guard(function):
     def decorator(*args, **kwargs):
         for _ in range(30):
             try:
                 return function(*args, **kwargs)
-            except binance.exceptions.BinanceAPIException as api_exception:
+            except (requests.exceptions.ConnectionError, binance.exceptions.BinanceAPIException) as api_exception:
                 logging.error(
                     "Exception occured during exchange operation %s: %s",
                     function.__name__, str(api_exception))
+                time.sleep(5)
         return False
 
     return decorator
--- mailing/postman.py
+++ mailing/postman.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import time
+import logging
+
 import smtplib
 from email.message import EmailMessage
 
@@ -15,8 +18,6 @@ class Postman:
     def connect(self):
         self.server = smtplib.SMTP(self.configuration.smtp_server,
                                    self.configuration.port)
-        self.server.login(self.configuration.sender,
-                          self.configuration.password)
 
     def send(self, message: mailing.message.Message) -> bool:
         msg = EmailMessage()
@@ -24,4 +25,15 @@ class Postman:
         msg['From'] = self.configuration.sender
         msg['To'] = self.configuration.receiver
         msg.set_content(message.get())
-        self.server.send_message(msg)
+        for i in range(10):
+            try:
+                self.server.login(self.configuration.sender,
+                                  self.configuration.password)
+                self.server.send_message(msg)
+                self.server.quit()
+                return True
+            except (OSError, smtplib.SMTPException, smtplib.SMTPSenderRefused) as exc:
+                logging.exception(exc)
+                logging.warning("Could not send mail. Retrying after 10 secs.")
+                time.sleep(10)
+        return False
