7b4d92c0a93ad72a802d5c27adcbf024c04a2921
Balázs János Varga <vargab95@gmail.com>
2020-03-25 17:07:14 +0100

    Add env

--- trading-view-bot-ui/src/app/indicator.service.ts
+++ trading-view-bot-ui/src/app/indicator.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IndicatorService {
-  private baseUrl = 'http://localhost:5000/indicator';
+  private baseUrl = environment.apiBaseUrl + 'indicator';
 
   constructor(private http: HttpClient) {}
 
