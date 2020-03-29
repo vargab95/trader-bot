264f383296910a260122347d4de8895444f0ea4f
Balázs János Varga <vargab95@gmail.com>
2020-03-30 00:29:13 +0200

    Refactor forms.

--- trading-view-bot-ui/src/app/add-indicator-form/add-indicator-form.component.ts
+++ trading-view-bot-ui/src/app/add-indicator-form/add-indicator-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { IndicatorService, IndicatorRequest } from '../indicator.service';
+import { ChartDataService } from '../chart-data.service';
 
 @Component({
   selector: 'app-add-indicator-form',
@@ -6,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-indicator-form.component.css']
 })
 export class AddIndicatorFormComponent implements OnInit {
+  addIndicatorForm: FormGroup;
 
-  constructor() { }
+  indicatorMarkets = [];
+  indicatorNames = [];
+  indicatorCandleSizes = [];
 
-  ngOnInit(): void {
+  constructor(
+    private indicatorService: IndicatorService,
+    private formBuilder: FormBuilder,
+    private chartDataService: ChartDataService
+  ) {}
+
+  ngOnInit() {
+    this.indicatorService.getOptions().subscribe(response => {
+      this.indicatorMarkets = response.market;
+      this.indicatorNames = response.indicator;
+      this.indicatorCandleSizes = response.candle_size;
+    });
+
+    this.addIndicatorForm = this.formBuilder.group({
+      market: '',
+      indicator: '',
+      candleSize: '',
+      dateSpan: {
+        start: '',
+        end: ''
+      },
+      limit: -1,
+      color: '225,10,20',
+      sma: 0
+    });
   }
 
+  onAddIndicator(event) {
+    console.log(event);
+
+    const request: IndicatorRequest = {
+      market: event.market,
+      indicator: event.indicator,
+      candle_size: event.candleSize,
+      start_date: event.dateSpan.start.toISOString(),
+      end_date: event.dateSpan.end.toISOString(),
+      sma: event.sma
+    };
+    this.indicatorService.getIndicators(request).subscribe(response => {
+      this.chartDataService.addChart(
+        response,
+        event.color,
+        event.market + '.' + event.indicator + '.' + event.candleSize
+      );
+    });
+  }
 }
--- trading-view-bot-ui/src/app/add-ticker-form/add-ticker-form.component.ts
+++ trading-view-bot-ui/src/app/add-ticker-form/add-ticker-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { TickerService, TickerRequest } from '../ticker.service';
+import { ChartDataService } from '../chart-data.service';
 
 @Component({
   selector: 'app-add-ticker-form',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-ticker-form.component.css']
 })
 export class AddTickerFormComponent implements OnInit {
+  tickerMarkets = [];
+  addTickerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private tickerService: TickerService,
+    private formBuilder: FormBuilder,
+    private chartDataService: ChartDataService
+  ) {}
 
   ngOnInit(): void {
+    this.tickerService.getOptions().subscribe(response => {
+      this.tickerMarkets = response.market;
+    });
+
+    this.addTickerForm = this.formBuilder.group({
+      market: '',
+      dateSpan: {
+        start: '',
+        end: ''
+      },
+      limit: -1,
+      color: '225,10,20',
+      sma: 0
+    });
   }
 
+  onAddTicker(event) {
+    console.log(event);
+
+    const request: TickerRequest = {
+      market: event.market,
+      start_date: event.dateSpan.start.toISOString(),
+      end_date: event.dateSpan.end.toISOString(),
+      sma: event.sma
+    };
+    this.tickerService.getTickers(request).subscribe(response => {
+      this.chartDataService.addChart(response, event.color, event.market);
+    });
+  }
 }
