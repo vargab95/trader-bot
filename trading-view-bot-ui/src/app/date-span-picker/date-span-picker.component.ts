535a767028940a02e8ea52ac643a290e8bdaa4ab
Balázs János Varga <vargab95@gmail.com>
2020-03-30 00:29:13 +0200

    Refactor forms.

--- trading-view-bot-ui/src/app/date-span-picker/date-span-picker.component.ts
+++ trading-view-bot-ui/src/app/date-span-picker/date-span-picker.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR
+} from '@angular/forms';
 
 @Component({
   selector: 'app-date-span-picker',
   templateUrl: './date-span-picker.component.html',
-  styleUrls: ['./date-span-picker.component.css']
+  styleUrls: ['./date-span-picker.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DateSpanPickerComponent),
+      multi: true
+    }
+  ]
 })
-export class DateSpanPickerComponent implements OnInit {
+export class DateSpanPickerComponent implements OnInit, ControlValueAccessor {
+  @Input() start = new Date();
+  @Input() end = new Date();
 
-  constructor() { }
+  dateSpanForm: FormGroup;
+  propagateChange = (_: DateSpan) => {};
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.dateSpanForm = new FormGroup({
+      startDate: new FormControl(),
+      endDate: new FormControl()
+    });
+
+    this.dateSpanForm.get('startDate').valueChanges.subscribe(value => {
+      this.start = value;
+      this.propagateChange({ start: this.start, end: this.end } as DateSpan);
+    });
+
+    this.dateSpanForm.get('endDate').valueChanges.subscribe(value => {
+      this.end = value;
+      this.propagateChange({ start: this.start, end: this.end } as DateSpan);
+    });
   }
 
+  writeValue(value: DateSpan) {
+    this.start = value.start;
+    this.end = value.end;
+  }
+
+  registerOnChange(fn) {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched() {}
+}
+
+export interface DateSpan {
+  start: Date;
+  end: Date;
 }
