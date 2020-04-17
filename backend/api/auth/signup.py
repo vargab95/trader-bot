758dd0747dd81e3413e2f7f07636c0214764b965
Balázs János Varga <vargab95@gmail.com>
2020-04-17 21:41:02 +0200

    Implement auth APIs. refs #794

---
+++ backend/api/auth/signup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from flask_restful import Resource, reqparse
+from flask_bcrypt import generate_password_hash
+
+import storage.user
+
+
+class SignupApi(Resource):
+    user_storage: storage.user.UserStorage = None
+
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+
+        self.parser.add_argument('email', type=str, required=True)
+        self.parser.add_argument('password', type=str, required=True)
+
+    def post(self):
+        request = self.parser.parse_args()
+        try:
+            self.user_storage.add(request["email"],
+                                  generate_password_hash(request["password"]))
+            return {}, 200
+        except storage.user.InvalidUserRequest:
+            return {"message": "E-mail already exists"}, 409
