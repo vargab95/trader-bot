d24e1b169beab4cc017b46535443a021f5942e98
Balázs János Varga <vargab95@gmail.com>
2020-04-17 11:31:34 +0200

    Implement unit tests for derivative. refs #859

---
+++ backend/filters/ut_derivative.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import unittest
+
+import filters.derivative
+
+
+class DerivativeTest(unittest.TestCase):
+    def test_fill_up(self):
+        derivative = filters.derivative.Derivative(10)
+
+        for _ in range(0, 9):
+            derivative.put(1.0)
+            self.assertIsNone(derivative.get())
+
+        derivative.put(1.0)
+        self.assertAlmostEqual(derivative.get(), 0.0)
+
+    def test_delayed_calculation(self):
+        derivative = filters.derivative.Derivative(10)
+
+        for i in range(0, 9):
+            derivative.put(1.0 if i % 2 else 2.0)
+            self.assertIsNone(derivative.get())
+
+        derivative.put(1.0)
+        self.assertAlmostEqual(derivative.get(), -1.0)
+
+    def test_zero_derivative(self):
+        derivative = filters.derivative.Derivative(2)
+
+        derivative.put(1.0)
+        derivative.put(1.0)
+
+        self.assertAlmostEqual(derivative.get(), 0.0)
+
+    def test_increasing_derivative(self):
+        derivative = filters.derivative.Derivative(2)
+
+        derivative.put(1.1)
+        derivative.put(1.2)
+
+        self.assertAlmostEqual(derivative.get(), 0.1)
+
+    def test_decreasing_derivative(self):
+        derivative = filters.derivative.Derivative(2)
+
+        derivative.put(1.2)
+        derivative.put(1.1)
+
+        self.assertAlmostEqual(derivative.get(), -0.1)
