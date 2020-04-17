ab9b17a7a3e9978e39bd313504beca426092a7cb
Balázs János Varga <vargab95@gmail.com>
2020-04-17 21:14:27 +0200

    Implement complex filter factory and corresponding unittests. refs #859

--- backend/filters/ut_factory.py
+++ backend/filters/ut_factory.py
@@ -7,6 +7,7 @@ import filters.sma
 import filters.wma
 import filters.hma
 import filters.derivative
+import filters.complex
 
 
 class FilterFactoryTest(unittest.TestCase):
@@ -25,6 +26,33 @@ class FilterFactoryTest(unittest.TestCase):
     def test_create_derivative(self):
         instance = filters.factory.FilterFactory.create("derivative")
         self.assertTrue(isinstance(instance, filters.derivative.Derivative))
+
+    def test_create_complex(self):
+        instance = filters.factory.FilterFactory.create_complex({
+            "sma": 5,
+            "derivative": 2
+        })
+        self.assertTrue(isinstance(instance, filters.complex.Complex))
+
+    def test_create_complex_invalid_type(self):
+        try:
+            filters.factory.FilterFactory.create_complex({
+                "invalid": 4,
+                "sma": 5
+            })
+            self.fail()
+        except filters.factory.InvalidFilterFactoryParameter:
+            pass
+
+    def test_create_complex_invalid_length(self):
+        try:
+            filters.factory.FilterFactory.create_complex({
+                "sma": 5,
+                "derivative": -1
+            })
+            self.fail()
+        except filters.factory.InvalidFilterFactoryParameter:
+            pass
 
     def test_invalid_param_filter_type(self):
         try:
