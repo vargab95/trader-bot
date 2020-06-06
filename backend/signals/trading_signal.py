0a1d7ebb42b5ef5eb444b2768af822723272ae59
Balázs János Varga <vargab95@gmail.com>
2020-06-06 15:40:19 +0200

    Create dataclasses for trading signals

---
+++ backend/signals/trading_signal.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import datetime
+import typing
+from dataclasses import dataclass
+
+
+@dataclass
+class TradingSignalPoint:
+    value: float = 0.0
+    date: datetime.datetime = None
+
+
+@dataclass
+class TradingSignalDescriptor:
+    market: str = ""
+    start_date: datetime.datetime = None
+    end_date: datetime.datetime = None
+    limit: int = -1
+    step: int = 1
+
+
+@dataclass
+class IndicatorSignalDescriptor(TradingSignalDescriptor):
+    indicator: str = ""
+    candle_size: str = ""
+
+
+@dataclass
+class TickerSignalDescriptor(TradingSignalDescriptor):
+    pass
+
+
+@dataclass
+class TradingSignal:
+    data: typing.List[TradingSignalPoint]
+    descriptor: TradingSignalDescriptor
