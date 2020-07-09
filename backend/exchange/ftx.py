c5ad65b0ea710d3b2ebc4c35aa000871c5921456
Balázs János Varga <vargab95@gmail.com>
2020-07-09 22:29:15 +0200

    Fix datetime formatting in ftx history request.

--- backend/exchange/ftx.py
+++ backend/exchange/ftx.py
@@ -145,11 +145,13 @@ class FtxController(exchange.base.ExchangeBase):
 
         if descriptor.start_date is not None:
             request_url += "&start_time=" + \
-                descriptor.start_date.strftime(self.datetime_format)
+                str((descriptor.start_date
+                     - datetime.datetime(1970, 1, 1)).total_seconds() * 1000)
 
         if descriptor.end_date is not None:
             request_url += "&end_time=" + \
-                descriptor.end_date.strftime(self.datetime_format)
+                str((descriptor.end
+                     - datetime.datetime(1970, 1, 1)).total_seconds() * 1000)
 
         logging.debug("FTX price history request: %s", request_url)
         response = requests.get(request_url)
