869e23087604fff5d2f4b213a874f5107ff60bb8
Balázs János Varga <vargab95@gmail.com>
2021-07-16 22:00:05 +0200

    Remove meaningless test cases

--- backend/config/test_config.py
+++ backend/config/test_config.py
@@ -11,25 +11,6 @@ import config.common
 
 
 class ConfigTest(unittest.TestCase):
-    # This test is only for checking typos
-    def test_run_on_all_lines(self):
-        self.assertNotEqual(str(config.application.ApplicationConfig({})), "")
-
-    def test_run_simulator_on_all_lines(self):
-        self.assertNotEqual(str(config.simulator.SimulatorConfig({})), "")
-
-    def test_run_detector_on_all_lines(self):
-        self.assertNotEqual(str(config.detector.DetectorConfig({})), "")
-
-    def test_run_filter_on_all_lines(self):
-        self.assertNotEqual(str(config.filter.FilterConfig({})), "")
-
-    def test_parse(self):
-        parser = config.parser.ConfigurationParser()
-        parser.read("conf.yml")
-        self.assertIsInstance(parser.configuration,
-                              config.application.ApplicationConfig)
-
     def test_parse_fail(self):
         parser = config.parser.ConfigurationParser()
         try:
