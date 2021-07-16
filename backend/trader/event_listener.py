73581a9672c80dfc67585bcb3c3c9134ae6f3f5f
Balázs János Varga <vargab95@gmail.com>
2021-07-16 10:49:18 +0200

    Implement detector signal integrator / listener in trader package

---
+++ backend/trader/event_listener.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from observer.subscriber import Subscriber
+from observer.event import SignalUpdatedEvent
+
+from detector.common import TradingAction
+
+
+class DetectorSignalListener(Subscriber):
+    def __init__(self):
+        self.__value: TradingAction = TradingAction.HOLD_SIGNAL
+
+    def update(self, event: SignalUpdatedEvent):
+        if event.value == TradingAction.BEARISH_SIGNAL:
+            self.__value = TradingAction.BEARISH_SIGNAL
+        elif event.value == TradingAction.BULLISH_SIGNAL:
+            self.__value = TradingAction.BULLISH_SIGNAL
+        elif event.value == TradingAction.HOLD_SIGNAL:
+            pass
+        else:
+            raise ValueError(f"{event.value} is not a valid trading action")
+
+    def read_and_clear(self) -> TradingAction:
+        value = self.__value
+        self.__value = TradingAction.HOLD_SIGNAL
+        return value
---
+++ backend/trader/test_event_listener.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import unittest
+
+from observer.event import SignalUpdatedEvent
+from detector.common import TradingAction
+
+from trader.event_listener import DetectorSignalListener
+
+
+class TestDetectorSignalListener(unittest.TestCase):
+    def setUp(self):
+        self.listener = DetectorSignalListener()
+
+    def test_single_hold_signal(self):
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.HOLD_SIGNAL))
+        self.assertEqual(self.listener.read_and_clear(), TradingAction.HOLD_SIGNAL)
+
+    def test_single_bullish_signal(self):
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.BULLISH_SIGNAL))
+        self.assertEqual(self.listener.read_and_clear(), TradingAction.BULLISH_SIGNAL)
+
+    def test_single_bearish_signal(self):
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.BEARISH_SIGNAL))
+        self.assertEqual(self.listener.read_and_clear(), TradingAction.BEARISH_SIGNAL)
+
+    def test_multiple_hold_signal(self):
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.HOLD_SIGNAL))
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.HOLD_SIGNAL))
+        self.assertEqual(self.listener.read_and_clear(), TradingAction.HOLD_SIGNAL)
+
+    def test_multiple_bullish_signal(self):
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.BULLISH_SIGNAL))
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.BULLISH_SIGNAL))
+        self.assertEqual(self.listener.read_and_clear(), TradingAction.BULLISH_SIGNAL)
+
+    def test_multiple_bearish_signal(self):
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.BEARISH_SIGNAL))
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.BEARISH_SIGNAL))
+        self.assertEqual(self.listener.read_and_clear(), TradingAction.BEARISH_SIGNAL)
+
+    def test_hold_after_bull(self):
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.BULLISH_SIGNAL))
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.HOLD_SIGNAL))
+        self.assertEqual(self.listener.read_and_clear(), TradingAction.BULLISH_SIGNAL)
+
+    def test_hold_after_bear(self):
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.BEARISH_SIGNAL))
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.HOLD_SIGNAL))
+        self.assertEqual(self.listener.read_and_clear(), TradingAction.BEARISH_SIGNAL)
+
+    def test_clear_on_read(self):
+        self.listener.update(SignalUpdatedEvent("TestSignal", TradingAction.BEARISH_SIGNAL))
+        self.assertEqual(self.listener.read_and_clear(), TradingAction.BEARISH_SIGNAL)
+        self.assertEqual(self.listener.read_and_clear(), TradingAction.HOLD_SIGNAL)
