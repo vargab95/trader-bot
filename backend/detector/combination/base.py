3fd291831d8f17fc30e59d6c3ed67ce1830f9fa3
Balázs János Varga <vargab95@gmail.com>
2021-07-23 20:07:21 +0200

    Remove publisher call from detector combination logic it will be implemented in the corresponding event listener to comply with the single responsibility principle

--- backend/detector/combination/base.py
+++ backend/detector/combination/base.py
@@ -3,31 +3,24 @@
 import abc
 import typing
 
-from observer.publisher import Publisher
-from observer.subscriber import Subscriber
-from observer.event import SignalUpdatedEvent
 from config.detector import DetectorCombinationConfig
 
 from detector.common import TradingAction
 
 
-class DetectorCombinationLogic(Subscriber):
-    def __init__(self, config: DetectorCombinationConfig, publisher: Publisher):
+class DetectorCombinationLogic:
+    def __init__(self, config: DetectorCombinationConfig):
         self.__config = config
         self.__last_signals: typing.Dict[str, TradingAction] = dict()
         self.__result: TradingAction = TradingAction.HOLD_SIGNAL
-        self.__publisher: Publisher = publisher
 
-    def update(self, event: SignalUpdatedEvent):
-        self.__last_signals[event.signal_name] = event.value
+    def update(self, signal_name: str, value: TradingAction):
+        self.__last_signals[signal_name] = value
 
         if len(self.__config.input_signal_ids) != len(self.__last_signals):
             return
 
         self.__result = self._calculate(self.__last_signals)
-
-        update_event = SignalUpdatedEvent(self.__config.output_signal_id, self.__result)
-        self.__publisher.notify_all_subscribers(update_event)
 
     @abc.abstractmethod
     def _calculate(self, signals: typing.Dict[str, TradingAction]) -> TradingAction:
--- backend/detector/combination/test_and_combination.py
+++ backend/detector/combination/test_and_combination.py
@@ -3,19 +3,14 @@
 import unittest
 
 from config.detector import DetectorCombinationConfig
-from observer.publisher import Publisher
-from observer.event import SignalUpdatedEvent
 
 from detector.combination.and_combination import DetectorAndCombination
 from detector.common import TradingAction
 
 
 class TestDetectorAndCombination(unittest.TestCase):
-    def setUp(self):
-        self.publisher = Publisher()
-        self.publisher.register_signal("result")
-
-    def __generate_and_detector(self, signal_ids=None):
+    @staticmethod
+    def __generate_and_detector(signal_ids=None):
         if signal_ids is None:
             signal_ids = ["signal1", "signal2"]
 
@@ -24,122 +19,122 @@ class TestDetectorAndCombination(unittest.TestCase):
             "output_signal_id": "result",
             "combination_type": "and"
         })
-        return DetectorAndCombination(config, self.publisher)
+        return DetectorAndCombination(config)
 
     def test_simple_and_combination_hold_and_hold(self):
         logic = self.__generate_and_detector()
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.HOLD_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.HOLD_SIGNAL))
+        logic.update("signal1", TradingAction.HOLD_SIGNAL)
+        logic.update("signal2", TradingAction.HOLD_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.HOLD_SIGNAL)
 
     def test_simple_and_combination_hold_and_bear(self):
         logic = self.__generate_and_detector()
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BEARISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.HOLD_SIGNAL))
+        logic.update("signal1", TradingAction.BEARISH_SIGNAL)
+        logic.update("signal2", TradingAction.HOLD_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.HOLD_SIGNAL)
 
     def test_simple_and_combination_bear_and_hold(self):
         logic = self.__generate_and_detector()
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.HOLD_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.BEARISH_SIGNAL))
+        logic.update("signal1", TradingAction.HOLD_SIGNAL)
+        logic.update("signal2", TradingAction.BEARISH_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.HOLD_SIGNAL)
 
     def test_simple_and_combination_bear_and_bear(self):
         logic = self.__generate_and_detector()
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BEARISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.BEARISH_SIGNAL))
+        logic.update("signal1", TradingAction.BEARISH_SIGNAL)
+        logic.update("signal2", TradingAction.BEARISH_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.BEARISH_SIGNAL)
 
     def test_simple_and_combination_multiple_bear(self):
         logic = self.__generate_and_detector(["signal1", "signal2", "signal3"])
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BEARISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.BEARISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal3", TradingAction.BEARISH_SIGNAL))
+        logic.update("signal1", TradingAction.BEARISH_SIGNAL)
+        logic.update("signal2", TradingAction.BEARISH_SIGNAL)
+        logic.update("signal3", TradingAction.BEARISH_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.BEARISH_SIGNAL)
 
     def test_simple_and_combination_multiple_bear_one_hold(self):
         logic = self.__generate_and_detector(["signal1", "signal2", "signal3"])
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BEARISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.HOLD_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal3", TradingAction.BEARISH_SIGNAL))
+        logic.update("signal1", TradingAction.BEARISH_SIGNAL)
+        logic.update("signal2", TradingAction.HOLD_SIGNAL)
+        logic.update("signal3", TradingAction.BEARISH_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.HOLD_SIGNAL)
 
     def test_simple_and_combination_hold_and_bull(self):
         logic = self.__generate_and_detector()
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BULLISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.HOLD_SIGNAL))
+        logic.update("signal1", TradingAction.BULLISH_SIGNAL)
+        logic.update("signal2", TradingAction.HOLD_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.HOLD_SIGNAL)
 
     def test_simple_and_combination_bull_and_hold(self):
         logic = self.__generate_and_detector()
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.HOLD_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.BULLISH_SIGNAL))
+        logic.update("signal1", TradingAction.HOLD_SIGNAL)
+        logic.update("signal2", TradingAction.BULLISH_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.HOLD_SIGNAL)
 
     def test_simple_and_combination_bull_and_bull(self):
         logic = self.__generate_and_detector()
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BULLISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.BULLISH_SIGNAL))
+        logic.update("signal1", TradingAction.BULLISH_SIGNAL)
+        logic.update("signal2", TradingAction.BULLISH_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.BULLISH_SIGNAL)
 
     def test_simple_and_combination_multiple_bull(self):
         logic = self.__generate_and_detector(["signal1", "signal2", "signal3"])
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BULLISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.BULLISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal3", TradingAction.BULLISH_SIGNAL))
+        logic.update("signal1", TradingAction.BULLISH_SIGNAL)
+        logic.update("signal2", TradingAction.BULLISH_SIGNAL)
+        logic.update("signal3", TradingAction.BULLISH_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.BULLISH_SIGNAL)
 
     def test_simple_and_combination_multiple_bull_one_hold(self):
         logic = self.__generate_and_detector(["signal1", "signal2", "signal3"])
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BULLISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.HOLD_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal3", TradingAction.BULLISH_SIGNAL))
+        logic.update("signal1", TradingAction.BULLISH_SIGNAL)
+        logic.update("signal2", TradingAction.HOLD_SIGNAL)
+        logic.update("signal3", TradingAction.BULLISH_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.HOLD_SIGNAL)
 
     def test_simple_and_combination_bear_and_bull(self):
         logic = self.__generate_and_detector()
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BEARISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.BULLISH_SIGNAL))
+        logic.update("signal1", TradingAction.BEARISH_SIGNAL)
+        logic.update("signal2", TradingAction.BULLISH_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.HOLD_SIGNAL)
 
     def test_simple_and_combination_multiple_bull_one_bear(self):
         logic = self.__generate_and_detector(["signal1", "signal2", "signal3"])
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BULLISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.BEARISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal3", TradingAction.BULLISH_SIGNAL))
+        logic.update("signal1", TradingAction.BULLISH_SIGNAL)
+        logic.update("signal2", TradingAction.BEARISH_SIGNAL)
+        logic.update("signal3", TradingAction.BULLISH_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.HOLD_SIGNAL)
 
     def test_simple_and_combination_one_from_all(self):
         logic = self.__generate_and_detector(["signal1", "signal2", "signal3"])
 
-        logic.update(SignalUpdatedEvent("signal1", TradingAction.BULLISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal2", TradingAction.BEARISH_SIGNAL))
-        logic.update(SignalUpdatedEvent("signal3", TradingAction.HOLD_SIGNAL))
+        logic.update("signal1", TradingAction.BULLISH_SIGNAL)
+        logic.update("signal2", TradingAction.BEARISH_SIGNAL)
+        logic.update("signal3", TradingAction.HOLD_SIGNAL)
 
         self.assertEqual(logic.read(), TradingAction.HOLD_SIGNAL)
