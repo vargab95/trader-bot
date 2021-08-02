6170da614ca6a9b59d6c9a410985ea4475e12f1f
Balázs János Varga <vargab95@gmail.com>
2021-08-03 00:07:09 +0200

    Implement tests for fetcher publisher

---
+++ backend/fetcher/test_publisher.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import unittest
+
+from config.fetcher import FetcherConfig
+from observer.publisher import Publisher
+from observer.subscriber import Subscriber
+from observer.event import SignalUpdatedEvent
+
+from fetcher.publisher import FetcherSignalPublisher
+from fetcher.single import TradingViewFetcherSingle
+
+
+class TestSubscriber(Subscriber):
+    def __init__(self):
+        self.value = None
+
+    def update(self, event: SignalUpdatedEvent):
+        self.value = event.value
+
+    def get(self):
+        return self.value
+
+
+@unittest.mock.patch("requests.post")
+class TestFilterEventListener(unittest.TestCase):
+    def test_publish(self, post_mock):
+        post_mock.return_value = unittest.mock.Mock()
+        post_mock.return_value.json.return_value = {
+            "data": [
+                {"d": [1.2]}
+            ]
+        }
+
+        subscriber = TestSubscriber()
+        publisher = Publisher()
+        instance = TradingViewFetcherSingle(FetcherConfig({
+            "market": "BTC-USD",
+            "candle_size": "1m",
+            "indicator": "all",
+            "check_interval": 60
+        }))
+        output_signal_id = "TestSignal"
+        listener = FetcherSignalPublisher(output_signal_id, instance, publisher)
+
+        publisher.register_signal(output_signal_id)
+        publisher.subscribe(output_signal_id, subscriber)
+
+        self.assertEqual(listener.read(), None)
+        self.assertEqual(subscriber.get(), None)
+        listener.publish()
+        self.assertEqual(listener.read(), 1.2)
+        self.assertAlmostEqual(subscriber.get(), 1.2)
