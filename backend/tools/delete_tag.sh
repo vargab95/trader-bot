846f93eedad1c8e16bab4e1b2adc9c3d103809a7
Balázs János Varga <vargab95@gmail.com>
2021-09-29 23:52:35 +0200

    Try to delete ephemeral docker tags

---
+++ backend/tools/delete_tag.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERNAME=$IMAGE_REGISTRY_CI_USER
+PASSWORD=$IMAGE_REGISTRY_CI_PASSWORD
+ORGANIZATION=$IMAGE_REGISTRY_CI_USER
+IMAGE=tradingviewbot
+TAG=$CI_COMMIT_SHA
+
+login_data() {
+cat <<EOF
+{
+  "username": "$USERNAME",
+  "password": "$PASSWORD"
+}
+EOF
+}
+
+TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "https://hub.docker.com/v2/users/login/" | jq -r .token`
+
+curl "https://hub.docker.com/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/${TAG}/" \
+-X DELETE \
+-H "Authorization: JWT ${TOKEN}"
