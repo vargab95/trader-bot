8e4e5e8aec7d4012212035a3d4c1909286881f99
Balázs János Varga <vargab95@gmail.com>
2021-10-01 14:40:04 +0200

    Fix cleanup job issue

--- backend/tools/tag-deleter/delete_tag.sh
+++ backend/tools/tag-deleter/delete_tag.sh
@@ -16,7 +16,4 @@ EOF
 }
 
 TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "https://hub.docker.com/v2/users/login/" | jq -r .token`
-
-curl "https://hub.docker.com/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/${TAG}/" \
--X DELETE \
--H "Authorization: JWT ${TOKEN}"
+curl "https://hub.docker.com/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/${TAG}/" -f -L -X DELETE -H "Authorization: Bearer ${TOKEN}"
