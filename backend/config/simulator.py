dbcd18242c5d5a836c52df0b9b933506f0381ed4
Balázs János Varga <vargab95@gmail.com>
2021-10-02 22:52:58 +0200

    Do not specify default pathes in simulator config

--- backend/config/simulator.py
+++ backend/config/simulator.py
@@ -7,11 +7,11 @@ from config.base import ConfigComponentBase
 
 class SimulatorConfig(ConfigComponentBase):
     def __init__(self, config: typing.Dict):
-        self.bullish_file_path: str = config.get("bullish_file_path", "")
-        self.bearish_file_path: str = config.get("bearish_file_path", "")
-        self.watched_file_path: str = config.get("watched_file_path", "")
-        self.log_output_path: str = config.get("log_output_path", "simulator_log.csv")
-        self.actions_output_path: str = config.get("actions_output_path", "simulator_actions.csv")
+        self.bullish_file_path: str = config.get("bullish_file_path", None)
+        self.bearish_file_path: str = config.get("bearish_file_path", None)
+        self.watched_file_path: str = config.get("watched_file_path", None)
+        self.log_output_path: str = config.get("log_output_path", None)
+        self.actions_output_path: str = config.get("actions_output_path", None)
 
     def validate(self):
         pass
