8e0801a67f01e0adccef84ce85c3e5b29b7a8a7a
Balázs János Varga <vargab95@gmail.com>
2021-10-03 22:47:00 +0200

    Increase minimum coverage

--- backend/measure_coverage.sh
+++ backend/measure_coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python3 -m coverage run --source=. --omit="*test_*,./tools*" -m unittest discover -p "test_*" &&
-    python3 -m coverage report --fail-under=80 -m | grep -vE "(100%)$"
+    python3 -m coverage report --fail-under=85 -m | grep -vE "(100%)$"
