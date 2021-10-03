ba46a810897e1b6150d49fdbce7b2db97be0c444
Balázs János Varga <vargab95@gmail.com>
2021-10-03 12:16:30 +0200

    Exclude tools from coverage measurement

--- backend/measure_coverage.sh
+++ backend/measure_coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 -m coverage run --source=. --omit="*test_*" -m unittest discover -p "test_*" &&
+python3 -m coverage run --source=. --omit="*test_*,./tools*" -m unittest discover -p "test_*" &&
     python3 -m coverage report --fail-under=80 -m
