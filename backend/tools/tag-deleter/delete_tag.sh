79aa4905bf5b2f2d3722f6365b21abcaeb6e376e
Balázs János Varga <vargab95@gmail.com>
2022-01-02 22:45:33 +0100

    Upload tag deleter to be able to remove tags from private registry

--- backend/tools/tag-deleter/delete_tag.sh
+++ backend/tools/tag-deleter/delete_tag.sh
@@ -5,6 +5,7 @@ PASSWORD=$IMAGE_REGISTRY_CI_PASSWORD
 ORGANIZATION=$IMAGE_REGISTRY_CI_USER
 IMAGE=$CI_PROJECT_NAME
 TAG=$CI_COMMIT_SHA
+URL=$IMAGE_REGISTRY_URL
 
 login_data() {
 cat <<EOF
@@ -15,5 +16,15 @@ cat <<EOF
 EOF
 }
 
-TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "https://hub.docker.com/v2/users/login/" | jq -r .token`
-curl "https://hub.docker.com/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/${TAG}/" -f -L -X DELETE -H "Authorization: Bearer ${TOKEN}"
+if [[ -z $USE_TOKEN ]]; then
+    curl --user $USER:$PASSWORD -X DELETE "https://${URL}/v2/${ORGANIZATION}/${IMAGE}/manifests/$(
+        curl --user $USER:$PASSWORD -I \
+            -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+            "https://${URL}/v2/${ORGANIZATION}/${IMAGE}/manifests/${TAG}" \
+        | awk '$1 == "docker-content-digest:" { print $2 }' \
+        | tr -d $'\r' \
+    )"
+else
+    TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "https://$URL/v2/users/login/" | jq -r .token`
+    curl "https://$URL/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/${TAG}/" -f -L -X DELETE -H "Authorization: Bearer ${TOKEN}"
+fi
